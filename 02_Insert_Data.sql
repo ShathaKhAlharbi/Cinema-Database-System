afdb25282f263bb2b1edcc012d72ec294cfc52e0
Shatha Alharbi <shaxtha@gmail.com>
2026-09-05 02:29:51 +0300

    Add initial data for Theatre and Screen tables
    Insert multiple records into Theatre and Screen tables.

---
+++ 02_Insert_Data.sql
@@ -0,0 +1,29 @@
+INSERT INTO Theatre VALUES
+('01', 'Muvicinemas', 1, 'Nakheel Plaza, Buraydah');
+
+INSERT INTO Theatre VALUES
+('02', 'VOX Cinemas', 1, 'HAIL SQUARE, HAIL');
+
+INSERT INTO Theatre VALUES
+('03', 'VOX Cinemas', 2, 'HAIL SQUARE, HAIL');
+
+INSERT INTO Theatre VALUES
+('04', 'AMC', 4, 'Riyadh Gallery 9, Riyadh');
+
+INSERT INTO Theatre VALUES
+('05', 'Muvicinemas', 3, 'Nakheel Mall, Dammam');
+
+INSERT INTO Screen
+VALUES ('001', 100, 200, '01');
+
+INSERT INTO Screen
+VALUES ('002', 80, 150, '02');
+
+INSERT INTO Screen
+VALUES ('003', 120, 180, '03');
+
+INSERT INTO Screen
+VALUES ('004', 90, 160, '04');
+
+INSERT INTO Screen
+VALUES ('005', 110, 210, '05');
